ce7c47b8d2f03d6120d65c520bb9e9f50f8d8bf1
hokagelegend2023 <136045532+hokagelegend2023@users.noreply.github.com>
2023-08-20 09:40:19 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,142 @@
+# SCRYPT LANGSUNG INSTAL DAN BANYAK TAMBAHAN ADD ONS BIAR MANTABZZZ... 
+
+# Order from me (MUST READ) before using
+
+</p> 
+<h2 align="center"> Supported Linux Distribution</h2>
+<p align="center"><img src="https://d33wubrfki0l68.cloudfront.net/5911c43be3b1da526ed609e9c55783d9d0f6b066/9858b/assets/img/debian-ubuntu-hover.png"></p> 
+<p align="center"><img src="https://img.shields.io/static/v1?style=for-the-badge&logo=debian&label=Debian%209&message=Stretch&color=purple"> <img src="https://img.shields.io/static/v1?style=for-the-badge&logo=debian&label=Debian%2010&message=Buster&color=purple">  <img src="https://img.shields.io/static/v1?style=for-the-badge&logo=ubuntu&label=Ubuntu%2018&message=Lts&color=red"> <img src="https://img.shields.io/static/v1?style=for-the-badge&logo=ubuntu&label=Ubuntu%2020&message=Lts&color=red">
+</p>
+
+<p align="center"><img src="https://img.shields.io/badge/Service-SSH_Over_Websocket-success.svg"> <img src="https://img.shields.io/badge/Service-SSH_UDP_Custom-success.svg"> <img src="https://img.shields.io/badge/Service-SSH_Dropbear-success.svg">  <img src="https://img.shields.io/badge/Service-Stunnel4-success.svg">  <img src="https://img.shields.io/badge/Service-Fail2Ban-brightgreen">  
+<p align="center"><img src="https://img.shields.io/badge/Service-XRAY_VLESS-success.svg">  <img src="https://img.shields.io/badge/Service-XRAY_VMESS-success.svg">  <img src="https://img.shields.io/badge/Service-XRAY_TROJAN-success.svg"> <img src= "https://img.shields.io/badge/Service-Websocket-success.svg"> <img src= "https://img.shields.io/badge/Service-GRPC-success.svg"> <img src= "https://img.shields.io/badge/Service-Shadowsocks-success.svg">  
+<p <p align="center"><img src="https://img.shields.io/badge/Service-Webmin-success.svg"> <img src="https://img.shields.io/badge/Service-Helium-success.svg">
+<p <p align="center"><img src="https://wangchujiang.com/sb/status/stable.svg">
+  
+# Required VPS is still fresh (MUST) / have never installed anything
+<br>
+- If you install the Script twice, you need to rebuild the VPS to factory settings, in the VPS provider panel<br>
+- DOMAIN (MUST) / Random<br>
+- DEBIAN 9/10<br>
+- Ubuntu 18/20 LTS<br>
+- CPU MIN 1 CORE<br>
+- RAM 1GB<br>
+- (Recommendation) Ubuntu 18 / 20 LTS (STABLE to use)
+- Tidak cocok di gunakan untuk UBUNTU 22.0 Banyak Bug Error
+<br>
+
+# Cloudflare settings for those who have their own Domain, you can check at folder [image](https://github.com/givpn/AutoScriptXray/tree/master/image) to display other settings
+<br>
+- SSL/TLS : FULL<br>
+- SSL/TLS Recommender : OFF<br>
+- GRPC : ON<br>
+- WEBSOCKET : ON<br>
+- Always Use HTTPS : OFF<br>
+- UNDER ATTACK MODE : OFF<br>
+<br>
+
+# Pointing
+![Pointing](https://raw.githubusercontent.com/givpn/AutoScriptXray/master/image/pointing.png)
+
+## Service & Port:
+<br>
+- OpenSSH                  : 22<br>
+- SSH Websocket            : 80<br>
+- SSH SSL Websocket        : 443<br>
+- Stunnel4                 : 222, 777<br>
+- Dropbear                 : 109, 143<br>
+- Badvpn                   : 7100-7900<br>
+- Nginx                    : 81<br>
+- Vmess WS TLS             : 443<br>
+- Vless WS TLS             : 443<br>
+- Trojan WS TLS            : 443<br>
+- Shadowsocks WS TLS       : 443<br>
+- Vmess WS none TLS        : 80<br>
+- Vless WS none TLS        : 80<br>
+- Trojan WS none TLS       : 80<br>
+- Shadowsocks WS none TLS  : 80<br>
+- Vmess gRPC               : 443<br>
+- Vless gRPC               : 443<br>
+- Trojan gRPC              : 443<br>
+- Shadowsocks gRPC         : 443<br>
+<br>
+  
+## Feature
+- Speedtest® by [Ookla®](https://speedtest.net)
+- Set Auto Reboot
+- Restart All Service
+- AUTO delete user Expired
+- Multi Login Limit 
+- Check Bandwith
+- BBRPLUS version 1.4.0 by [Chikage0o0](https://github.com/Chikage0o0) What is BBR [Search now BBR](https://www.google.com/search?q=what+bbr+in+linux)
+- DNS CHANGER
+- no auto backup? which... is permanently removed
+- Just accept the existing features / you can add them yourself manually
+- Additional Features (Optional) skipper (NOTE) install after [Step Install] is complete
+- Optional [install OpenVPN + Slowdns +](https://github.com/givpn/AutoScriptXray/tree/master/udp-custom) UDP-Custom by [Exe302](https://gitlab.com/Exe302) + Slowdns by [SL](https://github.com/fisabiliyusri)
+- Optional [install Panel Webmin + ADS Block](https://github.com/givpn/AutoScriptXray/tree/master/helium) Helium version 3.0 by [Abi Darwish](https://github.com/abidarwish)
+- Optional [install Bot Telegram Xolpanel](https://github.com/givpn/AutoScriptXray/tree/master/bot%20telegram%20panel) by [XolvaID](https://github.com/XolvaID)
+  
+
+
+# [Step Install]
+- Step 1 for (debian) please update first
+```
+apt update && apt upgrade -y && reboot
+```
+- Step 2 for (ubuntu) directly install
+```
+sysctl -w net.ipv6.conf.all.disable_ipv6=1 && sysctl -w net.ipv6.conf.default.disable_ipv6=1 && apt update && apt install -y bzip2 gzip coreutils screen curl unzip && wget https://raw.githubusercontent.com/hokagelegend2023/alpha3/main/setup.sh && chmod +x setup.sh && sed -i -e 's/\r$//' setup.sh && screen -S setup ./setup.sh
+```
+
+
+# install Helium ADS Block + Panel Webmin
+```
+apt update && apt install wget -y && wget -q -O /usr/bin/ins-helium "https://raw.githubusercontent.com/hokagelegend2023/alpha3/main/helium/ins-helium.sh" && chmod +x /usr/bin/ins-helium && ins-helium
+```
+
+# install udp-custom + slowdns + OpenVPN
+- (NOTE) Slowdns limit speed
+- Upload : 3 Mbps
+- Download : 3 Mbps
+```
+apt update && apt install wget -y && wget -qO- -O udp.sh "https://raw.githubusercontent.com/hokagelegend2023/alpha3/main/udp-custom/udp.sh" && chmod +x udp.sh && ./udp.sh
+```
+- NS Pointing
+![Service Status](https://raw.githubusercontent.com/givpn/AutoScriptXray/master/udp-custom/slowdns/nspointing.png)
+
+
+
+# Mendapat Hak Asses root :
+
+- Pertama Login VPS
+- kemudian masukan kode perintah di bawah ini
+- sudo su
+- cd
+- nano /etc/ssh/sshd_config
+- ----  PermitRootLogin prohibit-password menjadi
+- PermitRootLogin yes
+
+- ------ PasswordAuthentication no, ubah menjadi
+- PasswordAuthentication yes
+- 
+- Simpan file dengan cara tekan Ctrl x lalu y lalu Enter
+- ketikan
+  systemctl restart ssh
+  systemctl restart sshd
+- kemudian ganti password akses root dengan memasukan kode di bawah ini
+- Ketikan 
+  passwd root
+- Masukan Pasword nya lalu
+- Ketikan perintah
+  service ssh restart
+  service sshd restart
+  
+
+# UNTUK ID CLOUD HOST GUNAKAN DEBIAN 10
+
+<P>
+</p> 
+<p align="center"><img src="https://img.shields.io/static/v1?style=for-the-badge&logo=debian&label=Debian%209&message=Stretch&color=purple"> <img src="https://img.shields.io/static/v1?style=for-the-badge&logo=debian&label=Debian%2010&message=Buster&color=purple">
+</P>
+  
---
+++ setup.sh
@@ -0,0 +1,341 @@
+#!/bin/bash
+# cari apa..?? harta tahta hanya sementara ingat masih ada kehidupan setelah kematian
+# jangan lupa sholat ingat ajal menantimu
+# dibawah ini bukan cd kaset ya
+cd
+rm -rf setup.sh
+clear
+red='\e[1;31m'
+green='\e[0;32m'
+yell='\e[1;33m'
+tyblue='\e[1;36m'
+BRed='\e[1;31m'
+BGreen='\e[1;32m'
+BYellow='\e[1;33m'
+BBlue='\e[1;34m'
+NC='\e[0m'
+purple() { echo -e "\\033[35;1m${*}\\033[0m"; }
+tyblue() { echo -e "\\033[36;1m${*}\\033[0m"; }
+yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+cd /root
+#System version number
+if [ "${EUID}" -ne 0 ]; then
+		echo "You need to run this script as root"
+  sleep 5
+		exit 1
+fi
+if [ "$(systemd-detect-virt)" == "openvz" ]; then
+		echo "OpenVZ is not supported"
+  clear
+                echo "For VPS with KVM and VMWare virtualization ONLY"
+  sleep 5
+		exit 1
+fi
+
+localip=$(hostname -I | cut -d\  -f1)
+hst=( `hostname` )
+dart=$(cat /etc/hosts | grep -w `hostname` | awk '{print $2}')
+if [[ "$hst" != "$dart" ]]; then
+echo "$localip $(hostname)" >> /etc/hosts
+fi
+# buat folder
+mkdir -p /etc/xray
+mkdir -p /etc/v2ray
+touch /etc/xray/domain
+touch /etc/v2ray/domain
+touch /etc/xray/scdomain
+touch /etc/v2ray/scdomain
+
+
+echo -e "[ ${BBlue}NOTES${NC} ] Before we go.. "
+sleep 0.5
+echo -e "[ ${BBlue}NOTES${NC} ] I need check your headers first.."
+sleep 0.5
+echo -e "[ ${BGreen}INFO${NC} ] Checking headers"
+sleep 0.5
+totet=`uname -r`
+REQUIRED_PKG="linux-headers-$totet"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+echo Checking for $REQUIRED_PKG: $PKG_OK
+if [ "" = "$PKG_OK" ]; then
+  sleep 0.5
+  echo -e "[ ${BRed}WARNING${NC} ] Try to install ...."
+  echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+  apt-get --yes install $REQUIRED_PKG
+  sleep 0.5
+  echo ""
+  sleep 0.5
+  echo -e "[ ${BBlue}NOTES${NC} ] If error you need.. to do this"
+  sleep 0.5
+  echo ""
+  sleep 0.5
+  echo -e "[ ${BBlue}NOTES${NC} ] apt update && apt upgrade -y && reboot"
+  sleep 0.5
+  echo ""
+  sleep 0.5
+  echo -e "[ ${BBlue}NOTES${NC} ] After this"
+  sleep 0.5
+  echo -e "[ ${BBlue}NOTES${NC} ] Then run this script again"
+  echo -e "[ ${BBlue}NOTES${NC} ] enter now"
+  read
+else
+  echo -e "[ ${BGreen}INFO${NC} ] Oke installed"
+fi
+
+ttet=`uname -r`
+ReqPKG="linux-headers-$ttet"
+if ! dpkg -s $ReqPKG  >/dev/null 2>&1; then
+  rm /root/setup.sh >/dev/null 2>&1 
+  exit
+else
+  clear
+fi
+
+
+secs_to_human() {
+    echo "Installation time : $(( ${1} / 3600 )) hours $(( (${1} / 60) % 60 )) minute's $(( ${1} % 60 )) seconds"
+}
+start=$(date +%s)
+ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
+sysctl -w net.ipv6.conf.all.disable_ipv6=1 >/dev/null 2>&1
+sysctl -w net.ipv6.conf.default.disable_ipv6=1 >/dev/null 2>&1
+
+echo -e "[ ${BGreen}INFO${NC} ] Preparing the install file"
+apt install git curl -y >/dev/null 2>&1
+apt install python -y >/dev/null 2>&1
+echo -e "[ ${BGreen}INFO${NC} ] Aight good ... installation file is ready"
+sleep 0.5
+echo -ne "[ ${BGreen}INFO${NC} ] Check permission : "
+
+echo -e "$BGreen Permission Accepted!$NC"
+sleep 2
+
+mkdir -p /var/lib/hokage/ >/dev/null 2>&1
+	echo "IP=" >> /var/lib/hokage/ipvps.conf
+
+echo ""
+clear
+echo -e "$BBlue                     SETUP DOMAIN VPS     $NC"
+echo -e "$BYellow----------------------------------------------------------$NC"
+echo -e "$BGreen 1. Use Domain Random / Gunakan Domain Random $NC"
+echo -e "$BGreen 2. Choose Your Own Domain / Gunakan Domain Sendiri $NC"
+echo -e "$BYellow----------------------------------------------------------$NC"
+read -rp " input 1 or 2 / pilih 1 atau 2 : " dns
+if test $dns -eq 1; then
+wget https://raw.githubusercontent.com/hokagelegend2023/alpha3/main/ssh/cf && chmod +x cf && ./cf
+elif test $dns -eq 2; then
+read -rp "Enter Your Domain / masukan domain : " dom
+echo "IP=" >> /var/lib/hokage/ipvps.conf
+echo "$dom" > /root/scdomain
+echo "$dom" > /etc/xray/scdomain
+echo "$dom" > /etc/xray/domain
+echo "$dom" > /etc/v2ray/domain
+echo "$dom" > /root/domain
+else 
+echo "Not Found Argument"
+exit 1
+fi
+echo -e "${BGreen}Done!${NC}"
+sleep 2
+clear
+mkdir -p /etc/hokagevpn/theme
+mkdir -p /var/lib/hokagevpn-pro >/dev/null 2>&1
+echo "IP=" >> /var/lib/hokagevpn-pro/ipvps.conf
+mkdir /var/lib/hokage;
+
+
+#THEME RED
+cat <<EOF>> /etc/hokagevpn/theme/red
+BG : \E[40;1;41m
+TEXT : \033[0;31m
+EOF
+#THEME BLUE
+cat <<EOF>> /etc/hokagevpn/theme/blue
+BG : \E[40;1;44m
+TEXT : \033[0;34m
+EOF
+#THEME GREEN
+cat <<EOF>> /etc/hokagevpn/theme/green
+BG : \E[40;1;42m
+TEXT : \033[0;32m
+EOF
+#THEME YELLOW
+cat <<EOF>> /etc/hokagevpn/theme/yellow
+BG : \E[40;1;43m
+TEXT : \033[0;33m
+EOF
+#THEME MAGENTA
+cat <<EOF>> /etc/hokagevpn/theme/magenta
+BG : \E[40;1;43m
+TEXT : \033[0;33m
+EOF
+#THEME CYAN
+cat <<EOF>> /etc/hokagevpn/theme/cyan
+BG : \E[40;1;46m
+TEXT : \033[0;36m
+EOF
+#THEME CONFIG
+cat <<EOF>> /etc/hokagevpn/theme/color.conf
+blue
+EOF
+#install ssh ovpn
+echo -e "\e[33m-----------------------------------\033[0m"
+echo -e "$BGreen      Install SSH Websocket           $NC"
+echo -e "\e[33m-----------------------------------\033[0m"
+sleep 0.5
+clear
+wget https://raw.githubusercontent.com/hokagelegend2023/alpha3/main/ssh/ssh-vpn.sh && chmod +x ssh-vpn.sh && ./ssh-vpn.sh
+#Instal Xray
+echo -e "\e[33m-----------------------------------\033[0m"
+echo -e "$BGreen          Install XRAY              $NC"
+echo -e "\e[33m-----------------------------------\033[0m"
+sleep 0.5
+clear
+wget https://raw.githubusercontent.com/hokagelegend2023/alpha3/main/xray/ins-xray.sh && chmod +x ins-xray.sh && ./ins-xray.sh
+clear
+wget https://raw.githubusercontent.com/hokagelegend2023/alpha3/main/sshws/insshws.sh && chmod +x insshws.sh && ./insshws.sh
+clear
+#Instal menu
+echo -e "\e[33m-----------------------------------\033[0m"
+echo -e "$BGreen     Install MENU UPDATE           $NC"
+echo -e "\e[33m-----------------------------------\033[0m"
+sleep 0.5
+clear
+wget https://raw.githubusercontent.com/hokagelegend2023/alpha3/main/update/updatemenu.sh && chmod +x updatemenu.sh && ./updatemenu.sh
+clear
+cat> /root/.profile << END
+# ~/.profile: executed by Bourne-compatible login shells.
+
+if [ "$BASH" ]; then
+  if [ -f ~/.bashrc ]; then
+    . ~/.bashrc
+  fi
+fi
+
+mesg n || true
+clear
+menu
+END
+chmod 644 /root/.profile
+
+if [ -f "/root/log-install.txt" ]; then
+rm /root/log-install.txt > /dev/null 2>&1
+fi
+if [ -f "/etc/afak.conf" ]; then
+rm /etc/afak.conf > /dev/null 2>&1
+fi
+if [ ! -f "/etc/log-create-ssh.log" ]; then
+echo "Log SSH Account " > /etc/log-create-ssh.log
+fi
+if [ ! -f "/etc/log-create-vmess.log" ]; then
+echo "Log Vmess Account " > /etc/log-create-vmess.log
+fi
+if [ ! -f "/etc/log-create-vless.log" ]; then
+echo "Log Vless Account " > /etc/log-create-vless.log
+fi
+if [ ! -f "/etc/log-create-trojan.log" ]; then
+echo "Log Trojan Account " > /etc/log-create-trojan.log
+fi
+if [ ! -f "/etc/log-create-shadowsocks.log" ]; then
+echo "Log Shadowsocks Account " > /etc/log-create-shadowsocks.log
+fi
+history -c
+serverV=$( curl -sS https://raw.githubusercontent.com/hokagelegend2023/akses/main/versi  )
+echo $serverV > /opt/.ver
+aureb=$(cat /home/re_otm)
+b=11
+if [ $aureb -gt $b ]
+then
+gg="PM"
+else
+gg="AM"
+fi
+curl -sS ipv4.icanhazip.com > /etc/myipvps
+echo " "
+echo "Installation has been completed!!"echo " "
+echo "===============================================================" | tee -a log-install.txt
+echo "" | tee -a log-install.txt
+echo "--------------------------------------------------------------" | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "====================================  | tee -a log-install.txt 
+echo "       HOKAGE LEGEND VPN PREMIUM | tee -a log-install.txt
+echo "------------------------------------------- | tee -a log-install.txt
+echo "   >>> Service & Port"  | tee -a log-install.txt
+echo "   - SlowDNS SSH             : ALL Port SSH"  | tee -a log-install.txt
+echo "   - OpenSSH                 : 22, 2253"  | tee -a log-install.txt
+echo "   - OpenVPN                 : TCP 1194"  | tee -a log-install.txt
+echo "   - OpenVPN                 : SSL  990"  | tee -a log-install.txt
+echo "   - OpenVPN                 : UDP  2200"  | tee -a log-install.txt
+echo "   - Stunnel5                : 443, 445"  | tee -a log-install.txt
+echo "   - Dropbear                : 443, 109, 143"  | tee -a log-install.txt
+echo "   - CloudFront Websocket    : "  | tee -a log-install.txt
+echo "   - SSH Websocket TLS       : 443"  | tee -a log-install.txt
+echo "   - SSH Websocket HTTP      : 8880"  | tee -a log-install.txt
+echo "   - Websocket OpenVPN       : 2086"  | tee -a log-install.txt
+echo "   - Squid Proxy             : 3128, 8080"  | tee -a log-install.txt
+echo "   - Badvpn                  : 7100, 7200, 7300"  | tee -a log-install.txt
+echo "   - Nginx                   : 89"  | tee -a log-install.txt
+echo "   - Wireguard               : 7070"  | tee -a log-install.txt
+echo "   - L2TP/IPSEC VPN          : 1701"  | tee -a log-install.txt
+echo "   - PPTP VPN                : 1732"  | tee -a log-install.txt
+echo "   - SSTP VPN                : 444"  | tee -a log-install.txt
+echo "   - Shadowsocks-R           : 1443-1543"  | tee -a log-install.txt
+echo "   - SS-OBFS TLS             : 2443-2543"  | tee -a log-install.txt
+echo "   - SS-OBFS HTTP            : 3443-3543"  | tee -a log-install.txt
+echo "   - XRAYS Vmess TLS         : 8443"  | tee -a log-install.txt
+echo "   - XRAYS Vmess None TLS    : 80"  | tee -a log-install.txt
+echo "   - XRAYS Vless TLS         : 8443"  | tee -a log-install.txt
+echo "   - XRAYS Vless None TLS    : 80"  | tee -a log-install.txt
+echo "   - XRAYS Trojan            : 2083"  | tee -a log-install.txt
+echo "   - XRAYS VMESS GRPC        : 1180"  | tee -a log-install.txt
+echo "   - XRAYS VLESS GRPC        : 2280"  | tee -a log-install.txt
+echo "   - OHP SSH                 : 8181"  | tee -a log-install.txt
+echo "   - OHP Dropbear            : 8282"  | tee -a log-install.txt
+echo "   - OHP OpenVPN             : 8383"  | tee -a log-install.txt
+echo "   - TrojanGo                : 2087"  | tee -a log-install.txt
+echo "   - OpenSSH                  : 22"  | tee -a log-install.txt
+echo "   - SSH Websocket            : 80" | tee -a log-install.txt
+echo "   - SSH SSL Websocket        : 443" | tee -a log-install.txt
+echo "   - Stunnel4                 : 222, 777" | tee -a log-install.txt
+echo "   - Dropbear                 : 109, 143" | tee -a log-install.txt
+echo "   - Badvpn                   : 7100-7900" | tee -a log-install.txt
+echo "   - Nginx                    : 81" | tee -a log-install.txt
+echo "   - Vmess WS TLS             : 443" | tee -a log-install.txt
+echo "   - Vless WS TLS             : 443" | tee -a log-install.txt
+echo "   - Trojan WS TLS            : 443" | tee -a log-install.txt
+echo "   - Shadowsocks WS TLS       : 443" | tee -a log-install.txt
+echo "   - Vmess WS none TLS        : 80" | tee -a log-install.txt
+echo "   - Vless WS none TLS        : 80" | tee -a log-install.txt
+echo "   - Trojan WS none TLS       : 80" | tee -a log-install.txt
+echo "   - Shadowsocks WS none TLS  : 80" | tee -a log-install.txt
+echo "   - Vmess gRPC               : 443" | tee -a log-install.txt
+echo "   - Vless gRPC               : 443" | tee -a log-install.txt
+echo "   - Trojan gRPC              : 443" | tee -a log-install.txt
+echo "   - Shadowsocks gRPC         : 443" | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
+echo "   - Timezone                : Asia/Jakarta (GMT +7)"  | tee -a log-install.txt
+echo "   - Fail2Ban                : [ON]"  | tee -a log-install.txt
+echo "   - Dflate                  : [ON]"  | tee -a log-install.txt
+echo "   - IPtables                : [ON]"  | tee -a log-install.txt
+echo "   - Auto-Reboot             : [ON]"  | tee -a log-install.txt
+echo "   - IPv6                    : [OFF]"  | tee -a log-install.txt
+echo "   - Autoreboot On 05.00 GMT +7" | tee -a log-install.txt
+echo "   - Autobackup Data" | tee -a log-install.txt
+echo "   - Restore Data" | tee -a log-install.txt
+echo "   - Auto Delete Expired Account" | tee -a log-install.txt
+echo "   - Full Orders For Various Services" | tee -a log-install.txt
+echo "   - White Label" | tee -a log-install.txt
+echo "   - Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
+echo "  ====================================  | tee -a log-install.txt
+echo "       HOKAGE LEGEND VPN PREMIUM    | tee -a log-install.txt
+echo " -------------------------------------------  | tee -a log-install.txt
+echo " Reboot 15 Sec"
+sleep 15
+rm /root/setup.sh >/dev/null 2>&1
+rm /root/ins-xray.sh >/dev/null 2>&1
+rm /root/insshws.sh >/dev/null 2>&1
+rm /root/updatemenu.sh >/dev/null 2>&1
+reboot
